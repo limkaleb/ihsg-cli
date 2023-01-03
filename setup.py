916bd382c2eb2a3eef5a7a0418330532b139ab01
Kaleb Lim <limkaleb@gmail.com>
2023-01-03 19:02:39 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,8 +12,6 @@ setup(
   ],
   version='0.0.0',
   description='''The description of the package''',
-  long_description_content_type="text/markdown",
-  long_description=README,
   entry_points='''
   [console_scripts]
   ihsg=ihsgcli:ihsgcli
