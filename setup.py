05d87ac004433b72ed61c5860a22965ca161bcd6
Kaleb Lim <limkaleb@gmail.com>
2023-01-04 20:55:31 +0700

    bump version 0.0.3

--- setup.py
+++ setup.py
@@ -3,6 +3,8 @@ from setuptools import setup, find_packages
 setup(
   name='ihsginfo-cli',
   packages=find_packages(),
+  include_package_data=True,
+  py_modules=['ihsginfocli'],
   email='limkaleb@gmail.com',
   author='Lim Kaleb',
   install_requires=[
@@ -10,7 +12,7 @@ setup(
     'yfinance',
     'colorama'
   ],
-  version='0.0.2',
+  version='0.0.3',
   description='''The description of the package''',
   entry_points='''
   [console_scripts]
